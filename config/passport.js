64db895cc8aabdc6056fb5fb3690386ff834893a
afsal <afsalmohammed393@gmail.com>
2025-05-11 19:23:25 +0530

    fixed some issues with design

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.NODE_ENV === 'production' 
+        ? "https://www.revivo.live/auth/google/callback"
+        : "/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
